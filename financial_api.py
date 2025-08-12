17ee3c2c5d6d1328256390d441ea1affff783e13
ankon35 <ankon35744@gmail.com>
2025-08-13 03:56:23 +0600

    bug fixed

--- financial_api.py
+++ financial_api.py
@@ -129,7 +129,10 @@ app.mount("/UI", StaticFiles(directory="UI"), name="ui")
 # FastAPI Routes
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    """Serve the homepage"""
+    """Serve the homepage or redirect to login if not authenticated"""
+    user_id = request.cookies.get("user_id")
+    if not user_id:
+        return RedirectResponse(url="/login")
     import time
     return templates.TemplateResponse("index.html", {"request": request, "timestamp": int(time.time())})
 
